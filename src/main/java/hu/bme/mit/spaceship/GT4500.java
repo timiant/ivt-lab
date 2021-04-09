3881516d306caeb2e591524941bd368c33c68b9e
timiant <timian.tassi@gmail.com>
2021-04-09 10:50:34 +0200

    Task 4 - merge-conflict - Branch-B

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -16,7 +16,7 @@ public class GT4500 implements SpaceShip {
   }
 
   public boolean fireLaser(FiringMode firingMode) {
-    // TODO not implemented yet
+    // TODO not implemented yet - fireing with GT4500 spaceship
     return false;
   }
 
