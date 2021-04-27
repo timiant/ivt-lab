97cbd29ed43ad9c7a97e90173f308b7582f517b2
timiant <timian.tassi@gmail.com>
2021-04-27 19:35:32 +0200

    SolarLint TorpedoStore fix - secure random generator #2

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator = new Random();
+  private SecureRandom generator = new SecureRandom();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
